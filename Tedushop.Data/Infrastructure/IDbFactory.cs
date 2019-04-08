f1fc6a76969bbe49616586d652004798ac553d6d
nclong92 <nclong92@gmail.com>
2019-04-08 22:18:42 +0700

    Add entities framework code first for data

---
+++ Tedushop.Data/Infrastructure/IDbFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tedushop.Data.Infrastructure
+{
+    public interface IDbFactory: IDisposable
+    {
+        TeduShopDbContext Init();
+    }
+}
