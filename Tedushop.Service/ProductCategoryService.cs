038c2e312fbee7c0ab504d949c94b69e1988a545
longnc <nclong92@gmail.com>
2019-05-03 17:35:36 +0700

    searching for list

--- Tedushop.Service/ProductCategoryService.cs
+++ Tedushop.Service/ProductCategoryService.cs
@@ -18,6 +18,7 @@ namespace Tedushop.Service
         ProductCategory Delete(int id);
 
         IEnumerable<ProductCategory> GetAll();
+        IEnumerable<ProductCategory> GetAll(string keyword);
 
         IEnumerable<ProductCategory> GetAllByParentId(int parentId);
 
@@ -49,6 +50,19 @@ namespace Tedushop.Service
         public IEnumerable<ProductCategory> GetAll()
         {
             return _ProductCategoryRepository.GetAll();
+        }
+
+        public IEnumerable<ProductCategory> GetAll(string keyword)
+        {
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                return _ProductCategoryRepository.GetMulti(x => x.Name.Contains(keyword) || x.Description.Contains(keyword));
+            }
+            else
+            {
+                return _ProductCategoryRepository.GetAll();
+            }
+                
         }
 
         public IEnumerable<ProductCategory> GetAllByParentId(int parentId)
--- Tedushop.Web/app/components/product_categories/productCategoryListController.js
+++ Tedushop.Web/app/components/product_categories/productCategoryListController.js
@@ -5,18 +5,35 @@
 
     function productCategoryListController($scope, apiService) {
         $scope.productCategories = [];
-
+        $scope.page = 0;
+        $scope.pagesCount = 0;
         $scope.getProductCategories = getProductCategories;
+        $scope.keyword = '';
 
-        function getProductCategories() {
-            apiService.get('/api/productcategory/getall', null, function (result) {
-                $scope.productCategories = result.data;
+        $scope.search = search;
+
+        function search() {
+            getProductCategories();
+        }
+        function getProductCategories(page) {
+            page = page || 0;
+            var config = {
+                params: {
+                    keyword: $scope.keyword,
+                    page: page,
+                    pageSize: 20
+                }
+            }
+            apiService.get('/api/productcategory/getall', config, function (result) {
+                $scope.productCategories = result.data.Items;
+                $scope.page = result.data.Page;
+                $scope.pagesCount = result.data.TotalPages;
+                $scope.totalCount = result.data.TotalCount;
             }, function () {
-                console.log('Load Product Categories failed');
+                console.log('Load productcategory failed.');
             });
         }
 
-        $scope.getProductCategories();
-
-    }
-})(angular.module('tedushop.product_categories'));
+            $scope.getProductCategories();
+        }
+    })(angular.module('tedushop.product_categories'));
